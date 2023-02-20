48265d345a2437646bfb30fcf1eb985d1fa4b014
robin murphy <robinmurphy1@gmail.com>
2023-02-20 20:56:04 +0200

    added task

--- config/saprin/tasks.js
+++ config/saprin/tasks.js
@@ -1,1 +1,31 @@
-module.exports = [];
+module.exports = [
+    {
+        icon: 'wcg-cliemt.svg',
+        title: 'task.saprim-followup',
+        appliesTo: 'reports',
+        appliesToType: [ 'delivery' ],
+        actions: [ { form: 'saprin' } ],
+        events: [
+            {
+                id: 'saprin-followup-1',
+                days:7,
+                start:2,
+                end:2,
+            },
+            {
+                id: 'saprin-followup-2',
+                days:14,
+                start:2,
+                end:2,
+            }
+        ],
+        resolvedIf: function (contact, report, event, dueDate) {
+            return Utils.isFormSubmittedInWindow(
+                contact.reports,
+                'saprin',
+                Utils.addDate(dueDate, -event.start).getTime(),
+                Utils.addDate(dueDate, event.end + 1).getTime()
+            );
+        }
+    }
+];
