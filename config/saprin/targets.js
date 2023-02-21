30223fcf7170dcd85581ed5f64df6bfb896fedcf
robinm <robin.murphy@westerncape.go.za>
2023-02-21 08:27:14 +0200

    update target

--- config/saprin/targets.js
+++ config/saprin/targets.js
@@ -3,11 +3,11 @@ module.exports = [
     id: 'saprin-assessments-all-time',
     type: 'count',
     icon: 'wcg-household',
-    goal: -1,
+    goal: 10,
     translation_key: 'targets.assessments.title',
     subtitle_translation_key: 'targets.all_time.subtitle',
     appliesTo: 'reports',
-    appliesToType: ['assessment'],
+    appliesToType: ['assessment', 'saprin'],
     date: 'now'
   }
 
